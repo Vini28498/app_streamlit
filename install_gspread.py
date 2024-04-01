54a0908529e50a8075735df254d64fca7c2d049c
Vinícius Henrique de Oliveira <63620777+Vini28498@users.noreply.github.com>
2024-04-01 19:26:15 -0300

    Delete install_gspread.py

--- install_gspread.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-import sys
-
-# Use sys.executable para obter o caminho para o executável Python atual e execute o pip
-subprocess.run([sys.executable, "-m", "pip", "install", "gspread"])
